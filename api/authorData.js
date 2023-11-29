426791ceba7880eacd0db3e32faffae068f77fc7
Tion Blackmon <tionblackmon@icloud.com>
2023-11-28 18:02:48 -0600

    Snapshot

--- api/authorData.js
+++ api/authorData.js
@@ -3,7 +3,20 @@ import client from '../utils/client';
 const endpoint = client.databaseURL;
 
 // FIXME:  GET ALL AUTHORS
-const getAuthors = () => {};
+const getAuthors = () => new Promise((resolve, reject) => {
+  fetch(`${endpoint}/authors.json`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    // Will wait, then do this thing, then do this other thing. W/O .then, the code will
+    // run without getting the data. This tells it to wait.
+    .then((response) => response.json())
+    .then((data) => resolve(Object.values(data))) // Taking the data from the obj, resolve() turns/packages data
+    // into an array of things.
+    .catch(reject);
+});
 
 // FIXME: CREATE AUTHOR
 const createAuthor = () => {};
@@ -12,10 +25,35 @@ const createAuthor = () => {};
 const getSingleAuthor = () => {};
 
 // FIXME: DELETE AUTHOR
-const deleteSingleAuthor = () => {};
+const deleteSingleAuthor = (firebaseKey) => new Promise((resolve, reject) => {
+  fetch(`${endpoint}/authors/${firebaseKey}.json`, { // firebase key indicates what we want
+    // to del. W/o it, your entire db will be deleted.
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => resolve(data))
+    .catch(reject);
+});
 
 // FIXME: UPDATE AUTHOR
 const updateAuthor = () => {};
+
+// Filter Authors
+const favoriteAuthors = () => new Promise((resolve, reject) => {
+  // Filter by author, set it to true.
+  fetch(`${endpoint}/authors.json?orderBy="author"&equalTo=true`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => resolve(Object.values(data)))
+    .catch(reject);
+});
 
 // TODO: GET A SINGLE AUTHOR'S BOOKS
 const getAuthorBooks = () => {};
@@ -26,5 +64,6 @@ export {
   getSingleAuthor,
   deleteSingleAuthor,
   updateAuthor,
+  favoriteAuthors,
   getAuthorBooks,
 };

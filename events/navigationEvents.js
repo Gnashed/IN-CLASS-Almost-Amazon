ae9b6722cd1e4f0d0aad8382750d7624640197b7
Tion Blackmon <tion.blackmon@outlook.com>
2024-08-16 16:59:59 -0500

    As for CRUD I got READ to work by writing an asynchronous function that sends a GET request to my database URL.

--- events/navigationEvents.js
+++ events/navigationEvents.js
@@ -1,3 +1,5 @@
+import { getAuthors } from '../api/authorData';
+import { showAuthors } from '../pages/authors';
 import { signOut } from '../utils/auth';
 
 // navigation events
@@ -21,7 +23,7 @@ const navigationEvents = () => {
   // 2. Convert the response to an array because that is what the makeAuthors function is expecting
   // 3. If the array is empty because there are no authors, make sure to use the emptyAuthor function
   document.querySelector('#authors').addEventListener('click', () => {
-    console.warn('CLICKED AUTHORS');
+    getAuthors().then(showAuthors);
   });
 
   // STRETCH: SEARCH

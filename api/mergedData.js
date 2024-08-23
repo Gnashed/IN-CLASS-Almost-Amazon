e8749bd548a1b5865a06c0085e91cb5602f6d596
Tion Blackmon <tion.blackmon@outlook.com>
2024-08-23 06:42:29 -0500

    more comments.

--- api/mergedData.js
+++ api/mergedData.js
@@ -4,7 +4,7 @@ import { getSingleAuthor } from './authorData';
 // Get data for viewBook
 const getBookDetails = (firebaseKey) => new Promise((resolve, reject) => {
   // GET SINGLE BOOK
-  getSingleBook(firebaseKey).then((bookObject) => { // return single book object, make an API call to get the author information based on the returned book object from above.
+  getSingleBook(firebaseKey).then((bookObject) => { // return single book object, make an API call using this object.
     getSingleAuthor(bookObject.author_id) // we nest this promise so that we can use the book object
       .then((authorObject) => resolve({ ...bookObject, authorObject }));
   }).catch(reject);
@@ -14,7 +14,7 @@ const getBookDetails = (firebaseKey) => new Promise((resolve, reject) => {
 
 const getAuthorDetails = (firebaseKey) => new Promise((resolve, reject) => {
   // GET SINGLE AUTHOR
-  getSingleAuthor(firebaseKey).then((authorObject) => { // return single author object, make an API call to get the author information based on the returned author object from above.
+  getSingleAuthor(firebaseKey).then((authorObject) => { // return single author object, make an API call using this object.
     getSingleBook(authorObject.author_id) // we nest this promise so that we can use the author object
       .then((bookObject) => resolve({ ...authorObject, bookObject }));
   }).catch(reject);

3d98576aa71272df547496dd8b6d8ae74171117e
Tion Blackmon <tion.blackmon@outlook.com>
2024-08-24 07:09:54 -0500

    Changed authorObject.id to authorObject.firebaseKey.

--- api/mergedData.js
+++ api/mergedData.js
@@ -15,7 +15,9 @@ const getBookDetails = (firebaseKey) => new Promise((resolve, reject) => {
 const getAuthorDetails = (firebaseKey) => new Promise((resolve, reject) => {
   // GET SINGLE AUTHOR
   getSingleAuthor(firebaseKey).then((authorObject) => { // return single author object, make an API call using this object.
-    getAuthorBooks(authorObject.author_id) // we nest this promise so that we can use the author object from above.
+    // FIXME: FirebaseKey shows in console, but no data.
+    // console.warn(authorObject.firebaseKey);
+    getAuthorBooks(authorObject.firebaseKey) // we nest this promise so that we can use the author object from above.
       .then((bookObject) => resolve({ ...authorObject, bookObject }));
   }).catch(reject);
   // GET AUTHOR

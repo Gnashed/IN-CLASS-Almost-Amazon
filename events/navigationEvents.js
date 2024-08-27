1d1cf9ee226ff8e7ae0952fada1d4d836b6d364a
Tion Blackmon <tion.blackmon@outlook.com>
2024-08-27 08:59:10 -0500

    Tweaked the favoriteAuthors promise in /api to work with uid.

--- events/navigationEvents.js
+++ events/navigationEvents.js
@@ -29,7 +29,7 @@ const navigationEvents = (user) => {
     getAuthors(user.uid).then(showAuthors);
   });
   document.querySelector('#favorite-authors').addEventListener('click', () => {
-    favoriteAuthors().then(showAuthors);
+    favoriteAuthors(user).then(showAuthors);
   });
 
   // STRETCH: SEARCH

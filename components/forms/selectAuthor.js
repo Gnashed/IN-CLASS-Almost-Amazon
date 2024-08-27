c5ce9157c0e3b53fa929e070953cceb5e0071ef8
Tion Blackmon <tion.blackmon@outlook.com>
2024-08-26 21:09:26 -0500

    Added a comment.

--- components/forms/selectAuthor.js
+++ components/forms/selectAuthor.js
@@ -2,10 +2,13 @@ import { getAuthors } from '../../api/authorData';
 import renderToDOM from '../../utils/renderToDom';
 
 const selectAuthor = (authorId) => {
+  // First, save the auth'd user's uid.
+
   let domString = `<label for="author">Select an Author</label>
     <select class="form-control" id="author_id" required>
     <option value="">Select an Author</option>`;
 
+  // TODO: Figure out how to pass in user.id inside getAuthors()
   getAuthors().then((authorsArray) => {
     authorsArray.forEach((author) => {
       domString += `
